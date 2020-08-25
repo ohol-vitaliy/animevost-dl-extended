238b834ae2748700b105d2b5829e83dc42c42b64
Vitalii-Ohol <ohol.vitaliy@gmail.com>
2020-08-25 12:46:57 +0300

    fixed executable path in setup script

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Script to download anime from animevost.org
 
-Use `python3 animevost-dl.py -h` to see help
+Use `animevost-dl -h` to see help
 
+## Installation
+
+Just run
+
+``` bash
+curl https://raw.githubusercontent.com/ohol-vitaliy/animevost-dl-extended/master/setup.sh | bash
+```
 
--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@ destination="$HOME/.local/bin"
  
 if ! [ -d "$destination" ]; then
     mkdir -p "$destination"
-    export PATH="$destination/:$PATH"
+    echo 'export PATH="'"$destination"':$PATH"' >> $HOME/.bashrc
+    source "$HOME/.bashrc"
 fi
  
 wget -P "$destination/" "https://raw.githubusercontent.com/ohol-vitaliy/animevost-dl-extended/master/animevost-dl.py"
-chmod +x "$destination/animevost-dl.py"
+mv "$destination/animevost-dl.py" "$destination/animevost-dl"
+chmod +x "$destination/animevost-dl"
 yes | pip3 install requests python-slugify
